dd5653b2a69582ad5d00e5ce73fd690e153d0e12
JoseCorCab <josecordoba@uma.es>
2019-10-22 15:49:44 +0200

    Bug in mapping path fixed

--- aux_sh/compare_all_samples.sh
+++ aux_sh/compare_all_samples.sh
@@ -2,7 +2,7 @@
 source ~soft_bio_267/initializes/init_degenes_hunter
 REPORT_TEMPLATES_FOLDER=$CODE_PATH'/templates/reports'
 mkdir $HUNTER_RESULTS_FOLDER
-cat $MAPPING_HUNTER_RESULTS_FOLDER/*/metrics | sed "s/'//g" > $HUNTER_RESULTS_FOLDER'/all_metrics'
+cat $MAPPING_RESULTS_FOLDER/*/metrics | sed "s/'//g" > $HUNTER_RESULTS_FOLDER'/all_metrics'
 
 create_metric_table.rb $HUNTER_RESULTS_FOLDER'/all_metrics' sample $HUNTER_RESULTS_FOLDER'/metric_table'
 create_report.R -t $REPORT_TEMPLATES_FOLDER/alignments_report.Rmd -o $HUNTER_RESULTS_FOLDER/alignments_report.html -d $HUNTER_RESULTS_FOLDER/metric_table -H t
@@ -11,7 +11,7 @@ controls=${controls%?}
 treatments=`awk '{if($3 == "Treat") print $1}' $TARGET_FILE | tr "\n" ","`
 treatments=${treatments%?}
 ## Join all results of each sample in a general table
-maps2DEGhunter.rb $TARGET_FILE $MAPPING_HUNTER_RESULTS_FOLDER STAR_000*/selected_counts $HUNTER_RESULTS_FOLDER no
+maps2DEGhunter.rb $TARGET_FILE $MAPPING_RESULTS_FOLDER STAR_000*/selected_counts $HUNTER_RESULTS_FOLDER no
 grep -v '^N_' $HUNTER_RESULTS_FOLDER'/selected_counts' | sum_counts_by_isoform.rb - > $HUNTER_RESULTS_FOLDER'/final_counts.txt'
 
 #degenes_Hunter.R -i $HUNTER_RESULTS_FOLDER'/final_counts.txt' -C $controls -T $treatments -o $HUNTER_RESULTS_FOLDER 
