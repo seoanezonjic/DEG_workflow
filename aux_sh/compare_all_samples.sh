95c7a6b7e5e0a7254310754f7410541b08108b90
JoseCorCab <josecordoba@uma.es>
2019-10-23 15:40:36 +0200

    Execution using multiple target files added

--- aux_sh/compare_all_samples.sh
+++ aux_sh/compare_all_samples.sh
@@ -1,4 +1,8 @@
 #! /usr/bin/env bash
+#SBATCH --cpus=4
+#SBATCH --mem='4gb'
+#SBATCH --error=DEGenesHunter.%J.err
+#SBATCH --output=DEGenesHunter.%J.out
 source ~soft_bio_267/initializes/init_degenes_hunter
 REPORT_TEMPLATES_FOLDER=$CODE_PATH'/templates/reports'
 mkdir $HUNTER_RESULTS_FOLDER
@@ -6,13 +10,17 @@ cat $MAPPING_RESULTS_FOLDER/*/metrics | sed "s/'//g" > $HUNTER_RESULTS_FOLDER'/a
 
 create_metric_table.rb $HUNTER_RESULTS_FOLDER'/all_metrics' sample $HUNTER_RESULTS_FOLDER'/metric_table'
 create_report.R -t $REPORT_TEMPLATES_FOLDER/alignments_report.Rmd -o $HUNTER_RESULTS_FOLDER/alignments_report.html -d $HUNTER_RESULTS_FOLDER/metric_table -H t
-controls=`awk '{if($3 == "Ctrl") print $1}' $TARGET_FILE | tr "\n" ","`
-controls=${controls%?}
-treatments=`awk '{if($3 == "Treat") print $1}' $TARGET_FILE | tr "\n" ","`
-treatments=${treatments%?}
-## Join all results of each sample in a general table
-maps2DEGhunter.rb $TARGET_FILE $MAPPING_RESULTS_FOLDER STAR_000*/selected_counts $HUNTER_RESULTS_FOLDER no
-grep -v '^N_' $HUNTER_RESULTS_FOLDER'/selected_counts' | sum_counts_by_isoform.rb - > $HUNTER_RESULTS_FOLDER'/final_counts.txt'
+for TARGET_FILE in `echo $TARGET | tr "," " "`
+do
+	target_results_folder=$HUNTER_RESULTS_FOLDER'/'`echo $TARGET_FILE | sed 's/_target.txt//'`
+	mkdir $target_results_folder
+	controls=`awk '{if($3 == "Ctrl") print $1}' $TARGET_FILE | tr "\n" ","`;controls=${controls%?}
+	treatments=`awk '{if($3 == "Treat") print $1}' $TARGET_FILE | tr "\n" ","`;treatments=${treatments%?}
+	
+	## Join all results of each sample in a general table
+	maps2DEGhunter.rb $TARGET_FILE $MAPPING_RESULTS_FOLDER STAR_000*/selected_counts target_results_folder no
+	grep -v '^N_' target_results_folder'/selected_counts' | sum_counts_by_isoform.rb - > target_results_folder'/final_counts.txt'
 
-#degenes_Hunter.R -i $HUNTER_RESULTS_FOLDER'/final_counts.txt' -C $controls -T $treatments -o $HUNTER_RESULTS_FOLDER 
-degenes_Hunter.R -p $de_pvalue -m $de_packages -c $de_min_pack -i $HUNTER_RESULTS_FOLDER'/final_counts.txt' -C $controls -T $treatments -o $HUNTER_RESULTS_FOLDER -m 'DEN'
+	## Launch DEGenesHunter
+	degenes_Hunter.R -p $de_pvalue -m $de_packages -c $de_min_pack -i target_results_folder'/final_counts.txt' -C $controls -T $treatments -o target_results_folder -m 'DEN' &
+done
--- aux_sh/launch_fun_hun.sh
+++ aux_sh/launch_fun_hun.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env bash
-#SBATCH --cpus=1
+#SBATCH --cpus=4
 #SBATCH --mem='4gb'
 #SBATCH --time='10:00:00'
-#SBATCH --error=job.%J.err
-#SBATCH --output=job.%J.out
+#SBATCH --error=functionalHunter.%J.err
+#SBATCH --output=functionalHunter.%J.out
 
 source ~soft_bio_267/initializes/init_degenes_hunter
 
-#functional_Hunter.R -i ./Common_results/hunter_results_table.txt -c ./filtered_count_data.txt -m Human -G 'MBC' -t E 
-if [ $fun_remote_mode ]; then
-	functional_Hunter.R -f $fun_an_type -G $GO_modules -A $fun_an_performance -r $fun_remote_mode -T $fun_pvalue -i ./Common_results/hunter_results_table.txt -c ./filtered_count_data.txt -m Human -G 'MB' -t E -f 'G'
-else
-	functional_Hunter.R -f $fun_an_type -G $GO_modules -A $fun_an_performance -T $fun_pvalue -i ./Common_results/hunter_results_table.txt -c ./filtered_count_data.txt -m Human -G 'MB' -t E -f 'G'
-fi
+for TARGET_FILE in `echo $TARGET | tr "," " "`
+do
+	target_results_folder=$HUNTER_RESULTS_FOLDER'/'`echo $TARGET_FILE | sed 's/_target.txt//'`; cd $target_results_folder
+	#functional_Hunter.R -i ./Common_results/hunter_results_table.txt -c ./filtered_count_data.txt -m Human -G 'MBC' -t E 
+	if [ $fun_remote_mode ]; then
+		functional_Hunter.R -f $fun_an_type -G $GO_modules -A $fun_an_performance -r $fun_remote_mode -T $fun_pvalue -i ./Common_results/hunter_results_table.txt -c ./filtered_count_data.txt -m Human -G 'MB' -t E -f 'G' &
+	else
+		functional_Hunter.R -f $fun_an_type -G $GO_modules -A $fun_an_performance -T $fun_pvalue -i ./Common_results/hunter_results_table.txt -c ./filtered_count_data.txt -m Human -G 'MB' -t E -f 'G' &
+	fi
+done
