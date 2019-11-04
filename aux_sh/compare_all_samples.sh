7e0cdc845ed67023846cf44a1d860c7f8d175334
JoseCorCab <josecordoba@uma.es>
2019-11-04 16:24:23 +0100

    Resources optimized in DEGenes Hunter execution

--- aux_sh/compare_all_samples.sh
+++ aux_sh/compare_all_samples.sh
@@ -1,17 +1,17 @@
 #! /usr/bin/env bash
-#SBATCH --mem='4gb'
-#SBATCH --error=DEGenesHunter.%J.err
-#SBATCH --output=DEGenesHunter.%J.out
-source ~soft_bio_267/initializes/init_degenes_hunter
-REPORT_TEMPLATES_FOLDER=$CODE_PATH'/templates/reports'
-mkdir $HUNTER_RESULTS_FOLDER
-# cat $MAPPING_RESULTS_FOLDER/*/metrics | sed "s/'//g" > $HUNTER_RESULTS_FOLDER'/all_metrics'
 
-# create_metric_table.rb $HUNTER_RESULTS_FOLDER'/all_metrics' sample $HUNTER_RESULTS_FOLDER'/metric_table'
-# create_report.R -t $REPORT_TEMPLATES_FOLDER/alignments_report.Rmd -o $HUNTER_RESULTS_FOLDER/alignments_report.html -d $HUNTER_RESULTS_FOLDER/metric_table -H t
+source ~soft_bio_267/initializes/init_degenes_hunter
+
+
+mkdir $HUNTER_RESULTS_FOLDER
+cat $MAPPING_RESULTS_FOLDER/*/metrics | sed "s/'//g" > $HUNTER_RESULTS_FOLDER'/all_metrics'
+
+create_metric_table.rb $HUNTER_RESULTS_FOLDER'/all_metrics' sample $HUNTER_RESULTS_FOLDER'/metric_table'
+create_report.R -t $REPORT_TEMPLATES_FOLDER/alignments_report.Rmd -o $HUNTER_RESULTS_FOLDER/alignments_report.html -d $HUNTER_RESULTS_FOLDER/metric_table -H t
 for TARGET_FILE in `echo $TARGET | tr "," " "`
 do
-	target_results_folder=$HUNTER_RESULTS_FOLDER'/'`echo $TARGET_FILE | sed 's/_target.txt//'`
+	TARGET_NAME=`echo $TARGET_FILE | sed 's/_target.txt//'`
+	target_results_folder=$HUNTER_RESULTS_FOLDER'/'$TARGET_NAME
 	mkdir $target_results_folder
 	controls=`awk '{if($3 == "Ctrl") print $1}' $TARGET_FILE | tr "\n" ","`;controls=${controls%?}
 	treatments=`awk '{if($3 == "Treat") print $1}' $TARGET_FILE | tr "\n" ","`;treatments=${treatments%?}
@@ -22,6 +22,6 @@ do
 
 	## Launch DEGenesHunter
 	#echo "degenes_Hunter.R -p $de_pvalue -m $de_packages -c $de_min_pack -i $target_results_folder'/final_counts.txt' -C $controls -T $treatments -o $target_results_folder"
-	degenes_Hunter.R -p $de_pvalue -m $de_packages -c $de_min_pack -i $target_results_folder'/final_counts.txt' -C $controls -T $treatments -o $target_results_folder &
+	degenes_Hunter.R -f $de_logfc -p $de_pvalue -m $de_packages -c $de_min_pack -i $target_results_folder'/final_counts.txt' -C $controls -T $treatments -o $target_results_folder & 
 done
 wait
--- aux_sh/launch_fun_hun.sh
+++ aux_sh/launch_fun_hun.sh
@@ -1,18 +1,16 @@
 #!/usr/bin/env bash
-#SBATCH --mem='4gb'
-#SBATCH --time='10:00:00'
-#SBATCH --error=functionalHunter.%J.err
-#SBATCH --output=functionalHunter.%J.out
 
 source ~soft_bio_267/initializes/init_degenes_hunter
 
 for TARGET_FILE in `echo $TARGET | tr "," " "`
 do
-	target_results_folder=$HUNTER_RESULTS_FOLDER'/'`echo $TARGET_FILE | sed 's/_target.txt//'`; cd $target_results_folder
+	TARGET_NAME=`echo $TARGET_FILE | sed 's/_target.txt//'`
+	target_results_folder=$HUNTER_RESULTS_FOLDER'/'$TARGET_NAME; cd $target_results_folder
 	#functional_Hunter.R -i ./Common_results/hunter_results_table.txt -c ./filtered_count_data.txt -m Human -G 'MBC' -t E 
 	if [ $fun_remote_mode ]; then
-		functional_Hunter.R -f $fun_an_type -G $GO_modules -A $fun_an_performance -r $fun_remote_mode -T $fun_pvalue -i ./Common_results/hunter_results_table.txt -c ./filtered_count_data.txt -m Human -G 'MB' -t E -f 'G' &
+		functional_Hunter.R -f $fun_an_type -G $GO_modules -A $fun_an_performance -r $fun_remote_mode -T $fun_pvalue -i ./Common_results/hunter_results_table.txt -c ./filtered_count_data.txt -m Human -t E -o functional_enrichment 1>$logs'/'$CODE_PATH'/functional_hunter'_$TARGET_NAME'_local.out' 2>$logs'/'$CODE_PATH'/functional_hunter'_$TARGET_NAME'_local.err' &
 	else
-		functional_Hunter.R -f $fun_an_type -G $GO_modules -A $fun_an_performance -T $fun_pvalue -i ./Common_results/hunter_results_table.txt -c ./filtered_count_data.txt -m Human -G 'MB' -t E -f 'G' &
+		functional_Hunter.R -f $fun_an_type -G $GO_modules -A $fun_an_performance -T $fun_pvalue -i ./Common_results/hunter_results_table.txt -c ./filtered_count_data.txt -m Human -t E -o functional_enrichment &
 	fi
 done
+wait
