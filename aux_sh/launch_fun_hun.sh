64a8627cec51206ba624692e6847b825c14ace72
JoseCorCab <josecordoba@uma.es>
2019-11-06 13:24:52 +0100

    Bug in local mode fixed

--- aux_sh/launch_fun_hun.sh
+++ aux_sh/launch_fun_hun.sh
@@ -7,11 +7,10 @@ do
 	TARGET_NAME=`echo $TARGET_FILE | sed 's/_target.txt//'`
 	target_results_folder=$HUNTER_RESULTS_FOLDER'/'$TARGET_NAME; cd $target_results_folder
 	#functional_Hunter.R -i ./Common_results/hunter_results_table.txt -c ./filtered_count_data.txt -m Human -G 'MBC' -t E 
-	if [ $fun_remote_mode ]; then
-		functional_Hunter.R -f $fun_an_type -G $GO_modules -A $fun_an_performance -r $fun_remote_mode -T $fun_pvalue -i ./Common_results/hunter_results_table.txt -c ./filtered_count_data.txt -m Human -t E -o functional_enrichment 1>$logs'/'$CODE_PATH'/functional_hunter'_$TARGET_NAME'_local.out' 2>$logs'/'$CODE_PATH'/functional_hunter'_$TARGET_NAME'_local.err' &
+	if [ `echo $fun_remote_mode | grep -q -G "[kb]"` ]; then
+		functional_Hunter.R -f $fun_an_type -G $GO_modules -A $fun_an_performance -r $fun_remote_mode -T $fun_pvalue -i ./Common_results/hunter_results_table.txt -c ./filtered_count_data.txt -m Human -t E -o functional_enrichment &
 	else
-		#functional_Hunter.R -f $fun_an_type -G $GO_modules -A $fun_an_performance -T $fun_pvalue -i ./Common_results/hunter_results_table.txt -c ./filtered_count_data.txt -m Human -t E -o functional_enrichment &
-		/mnt/home/users/bio_267_uma/jabato/projects/degeneshunter_refactor/DEgenesHunter/functional_Hunter.R -f $fun_an_type -G $GO_modules -A $fun_an_performance -T $fun_pvalue -C /mnt/home/users/bio_267_uma/jabato/projects/degeneshunter_refactor/DEgenesHunter/samples/bcn_nomenclature -i ./Common_results/hunter_results_table.txt -c ./filtered_count_data.txt -m Human -t E -o functional_enrichment &
+		functional_Hunter.R -f $fun_an_type -G $GO_modules -A $fun_an_performance -T $fun_pvalue -i ./Common_results/hunter_results_table.txt -c ./filtered_count_data.txt -m Human -t E -o functional_enrichment &
 	fi
 done
 wait
