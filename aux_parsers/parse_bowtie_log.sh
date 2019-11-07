e045e96f774fa15da581b2bac277368de8f4d5aa
JoseCorCab <josecordoba@uma.es>
2019-11-07 09:38:07 +0100

    Bowtie log parser added

---
+++ aux_parsers/parse_bowtie_log.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+raw_report=$1
+
+echo -e "reads_suppresed_by_multimapping\t`grep 'reads with alignments suppressed due to -m' $raw_report | cut -f2 -d ':' | cut -f 2 -d ' '`"
+echo -e "unaligned_reads\t`grep 'failed to align' $raw_report | cut -f2 -d ':' | cut -f 2 -d ' '`"
+echo -e "aligned_reads\t`grep 'reads with at least one reported alignment' $raw_report | cut -f2 -d ':' | cut -f 2 -d ' '`"
+
+
+# reads processed: 13448817
+# reads with at least one reported alignment: 6208770 (46.17%)
+# reads that failed to align: 1997484 (14.85%)
+# reads with alignments suppressed due to -m: 5242563 (38.98%)
+#Reported 11322957 alignments to 1 output stream(s)
