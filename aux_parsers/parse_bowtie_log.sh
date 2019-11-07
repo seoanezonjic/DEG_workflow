313ec7e3ff621fc05065a150c22753e8537bb618
JoseCorCab <josecordoba@uma.es>
2019-11-07 11:01:33 +0100

    Alignment report for miRNA workflow

--- aux_parsers/parse_bowtie_log.sh
+++ aux_parsers/parse_bowtie_log.sh
@@ -3,8 +3,12 @@
 raw_report=$1
 
 echo -e "reads_suppresed_by_multimapping\t`grep 'reads with alignments suppressed due to -m' $raw_report | cut -f2 -d ':' | cut -f 2 -d ' '`"
+echo -e "multimapping_filtered_percentage\t`grep 'reads with alignments suppressed due to -m' $raw_report | cut -f2 -d ':' | cut -f 3 -d ' ' | tr -d "("| tr -d ")"| tr -d "%"`"
+
 echo -e "unaligned_reads\t`grep 'failed to align' $raw_report | cut -f2 -d ':' | cut -f 2 -d ' '`"
+echo -e "unaligned_reads_percentage\t`grep 'failed to align' $raw_report | cut -f2 -d ':' |  cut -f 3 -d ' ' | tr -d "("| tr -d ")"| tr -d "%"`"
 echo -e "aligned_reads\t`grep 'reads with at least one reported alignment' $raw_report | cut -f2 -d ':' | cut -f 2 -d ' '`"
+echo -e "aligned_reads_percentage\t`grep 'reads with at least one reported alignment' $raw_report | cut -f2 -d ':' |  cut -f 3 -d ' ' | tr -d "("| tr -d ")"| tr -d "%"`"
 
 
 # reads processed: 13448817
