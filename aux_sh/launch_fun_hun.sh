4046be7453a71c5eb5b31b9863af240ccc8b5e1f
JoseCorCab <josecordoba@uma.es>
2019-11-07 15:47:38 +0100

    Organism to perform functional analysis added to config_daemon

--- aux_sh/launch_fun_hun.sh
+++ aux_sh/launch_fun_hun.sh
@@ -7,9 +7,9 @@ do
 	TARGET_NAME=`echo $TARGET_FILE | sed 's/_target.txt//'`
 	target_results_folder=$HUNTER_RESULTS_FOLDER'/'$TARGET_NAME; cd $target_results_folder
 	if [ `echo $fun_remote_mode | grep -q -G "[kb]"` ]; then
-		functional_Hunter.R -C $custom_nomenclature -f $fun_an_type -G $GO_modules -A $fun_an_performance -r $fun_remote_mode -T $fun_pvalue -i ./Common_results/hunter_results_table.txt -c ./filtered_count_data.txt -m Human -t E -o functional_enrichment &
+		functional_Hunter.R -C $custom_nomenclature -f $fun_an_type -G $GO_modules -A $fun_an_performance -r $fun_remote_mode -T $fun_pvalue -i ./Common_results/hunter_results_table.txt -c ./filtered_count_data.txt -m $fun_organism -t E -o functional_enrichment &
 	else
-		functional_Hunter.R -C $custom_nomenclature -f $fun_an_type -G $GO_modules -A $fun_an_performance -T $fun_pvalue -i ./Common_results/hunter_results_table.txt -c ./filtered_count_data.txt -m Human -t E -o functional_enrichment &
+		functional_Hunter.R -C $custom_nomenclature -f $fun_an_type -G $GO_modules -A $fun_an_performance -T $fun_pvalue -i ./Common_results/hunter_results_table.txt -c ./filtered_count_data.txt -m $fun_organism -t E -o functional_enrichment &
 	fi
 done
 wait
