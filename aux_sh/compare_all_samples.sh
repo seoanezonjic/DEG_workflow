9e57a5fda5957dc7b6ce58c614ba4842f62b9879
JoseCorCab <josecordoba@uma.es>
2019-11-08 12:17:27 +0100

    Mapping report generation and DEGenesHunter execution separated by experiment_type option

--- aux_sh/compare_all_samples.sh
+++ aux_sh/compare_all_samples.sh
@@ -2,27 +2,28 @@
 
 source ~soft_bio_267/initializes/init_degenes_hunter
 
+mkdir $report_folder
+create_report.R -t $REPORT_TEMPLATES_FOLDER/alignments_report.Rmd -o $report_folder/alignments_report.html -d $HUNTER_RESULTS_FOLDER/metric_table -H t
 
-mkdir $HUNTER_RESULTS_FOLDER
-cat $MAPPING_RESULTS_FOLDER/*/metrics | sed "s/'//g" > $HUNTER_RESULTS_FOLDER'/all_metrics'
+if [[ $experiment_type == "RNAseq" ]]; then
+	mkdir $HUNTER_RESULTS_FOLDER
+	cat $MAPPING_RESULTS_FOLDER/*/metrics | sed "s/'//g" > $HUNTER_RESULTS_FOLDER'/all_metrics'
+	create_metric_table.rb $HUNTER_RESULTS_FOLDER'/all_metrics' sample $HUNTER_RESULTS_FOLDER'/metric_table'
+	for TARGET_FILE in `echo $TARGETS | tr "," " "`
+	do
+		TARGET_NAME=`echo $TARGET_FILE | sed 's/_target.txt//'`
+		target_results_folder=$HUNTER_RESULTS_FOLDER'/'$TARGET_NAME
+		mkdir $target_results_folder
+		controls=`awk '{if($3 == "Ctrl") print $1}' $TARGET_FILE | tr "\n" ","`;controls=${controls%?}
+		treatments=`awk '{if($3 == "Treat") print $1}' $TARGET_FILE | tr "\n" ","`;treatments=${treatments%?}
+		
+		## Join all results of each sample in a general table
+		maps2DEGhunter.rb $TARGET_FILE $MAPPING_RESULTS_FOLDER STAR_000*/selected_counts $target_results_folder no
+		grep -v '^N_' $target_results_folder'/selected_counts' | sum_counts_by_isoform.rb - > $target_results_folder'/final_counts.txt'
 
-create_metric_table.rb $HUNTER_RESULTS_FOLDER'/all_metrics' sample $HUNTER_RESULTS_FOLDER'/metric_table'
-create_report.R -t $REPORT_TEMPLATES_FOLDER/alignments_report.Rmd -o $HUNTER_RESULTS_FOLDER/alignments_report.html -d $HUNTER_RESULTS_FOLDER/metric_table -H t
-
-for TARGET_FILE in `echo $TARGETS | tr "," " "`
-do
-	TARGET_NAME=`echo $TARGET_FILE | sed 's/_target.txt//'`
-	target_results_folder=$HUNTER_RESULTS_FOLDER'/'$TARGET_NAME
-	mkdir $target_results_folder
-	controls=`awk '{if($3 == "Ctrl") print $1}' $TARGET_FILE | tr "\n" ","`;controls=${controls%?}
-	treatments=`awk '{if($3 == "Treat") print $1}' $TARGET_FILE | tr "\n" ","`;treatments=${treatments%?}
-	
-	## Join all results of each sample in a general table
-	maps2DEGhunter.rb $TARGET_FILE $MAPPING_RESULTS_FOLDER STAR_000*/selected_counts $target_results_folder no
-	grep -v '^N_' $target_results_folder'/selected_counts' | sum_counts_by_isoform.rb - > $target_results_folder'/final_counts.txt'
-
-	## Launch DEGenesHunter
-	#echo "degenes_Hunter.R -p $de_pvalue -m $de_packages -c $de_min_pack -i $target_results_folder'/final_counts.txt' -C $controls -T $treatments -o $target_results_folder"
-	degenes_Hunter.R -f $de_logfc -p $de_pvalue -m $de_packages -c $de_min_pack -i $target_results_folder'/final_counts.txt' -C $controls -T $treatments -o $target_results_folder & 
-done
-wait
+		## Launch DEGenesHunter
+		#echo "degenes_Hunter.R -p $de_pvalue -m $de_packages -c $de_min_pack -i $target_results_folder'/final_counts.txt' -C $controls -T $treatments -o $target_results_folder"
+		degenes_Hunter.R -f $de_logfc -p $de_pvalue -m $de_packages -c $de_min_pack -i $target_results_folder'/final_counts.txt' -C $controls -T $treatments -o $target_results_folder & 
+	done
+	wait
+fi
