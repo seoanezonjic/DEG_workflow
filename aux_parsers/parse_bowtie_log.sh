afc8003347069b00538759303d4837e4b2c66615
JoseCorCab <josecordoba@uma.es>
2019-11-22 09:55:49 +0100

    Metrics extraction for miRNAs modiffied

--- aux_parsers/parse_bowtie_log.sh
+++ aux_parsers/parse_bowtie_log.sh
@@ -2,17 +2,36 @@
 
 raw_report=$1
 
-echo -e "reads_suppresed_by_multimapping\t`grep 'reads with alignments suppressed due to -m' $raw_report | cut -f2 -d ':' | cut -f 2 -d ' '`"
-echo -e "multimapping_filtered_percentage\t`grep 'reads with alignments suppressed due to -m' $raw_report | cut -f2 -d ':' | cut -f 3 -d ' ' | tr -d "("| tr -d ")"| tr -d "%"`"
 
-echo -e "unaligned_reads\t`grep 'failed to align' $raw_report | cut -f2 -d ':' | cut -f 2 -d ' '`"
-echo -e "unaligned_reads_percentage\t`grep 'failed to align' $raw_report | cut -f2 -d ':' |  cut -f 3 -d ' ' | tr -d "("| tr -d ")"| tr -d "%"`"
-#echo -e "aligned_reads\t`grep 'reads with at least one reported alignment' $raw_report | cut -f2 -d ':' | cut -f 2 -d ' '`"
-echo -e "aligned_reads_percentage\t`grep 'reads with at least one reported alignment' $raw_report | cut -f2 -d ':' |  cut -f 3 -d ' ' | tr -d "("| tr -d ")"| tr -d "%"`"
+if `grep -q "overall alignment rate" $raw_report`; then
 
+	#unmapped=`grep 'aligned concordantly 0 times' $raw_report |sed -r 's/ +/ /' | cut -f 2 -d ' '` 
+	unmapped_per=`grep 'aligned 0 times' $raw_report |sed -r 's/ +/ /' | cut -f 3 -d ' '|tr -d ")" | tr -d "(" | tr -d "%"`
+	echo $unmapped_per
+	echo -e "bowtie2_unmapped_per\t"$unmapped_per
+	
+	mapped_uniq=`grep 'aligned exactly 1 time' $raw_report |sed -r 's/ +/ /' | cut -f 2 -d ' '`
+	mapped_uniq_per=`grep 'aligned exactly 1 time' $raw_report |sed -r 's/ +/ /' | cut -f 3 -d ' '|tr -d ")" | tr -d "(" | tr -d "%"`
+	echo -e "bowtie2_uniq_mapped\t$mapped_uniq"
+	echo -e "bowtie2_uniq_mapped_per\t$mapped_uniq_per"
 
-# reads processed: 13448817
-# reads with at least one reported alignment: 6208770 (46.17%)
-# reads that failed to align: 1997484 (14.85%)
-# reads with alignments suppressed due to -m: 5242563 (38.98%)
-#Reported 11322957 alignments to 1 output stream(s) 
+	multimapping=`grep 'aligned >1 times' $raw_report |sed -r 's/ +/ /' | cut -f 2 -d ' '`
+	multimapping_per=`grep 'aligned >1 times' $raw_report |sed -r 's/ +/ /' | cut -f 3 -d ' '|tr -d ")" | tr -d "(" | tr -d "%"`
+	echo -e "bowtie2_multimapping\t$multimapping"
+	echo -e "bowtie2_multimapping_per\t$multimapping_per"
+
+	all_mapping=`echo "$mapped_uniq + $multimapping" | bc`
+	echo -e "aligned_to_feature\t$all_mapping"
+	echo -e "bowtie2_mapped_reads\t$all_mapping"
+
+elif `grep -q "reads with at least one reported alignment" $raw_report`; then
+
+	echo -e "bowtie1_multimapping\t`grep 'reads with alignments suppressed due to -m' $raw_report | cut -f2 -d ':' | cut -f 2 -d ' '`"
+	echo -e "bowtie1_multimapping_per\t`grep 'reads with alignments suppressed due to -m' $raw_report | cut -f2 -d ':' | cut -f 3 -d ' ' | tr -d "("| tr -d ")"| tr -d "%"`"
+
+	echo -e "bowtie1_unmapped\t`grep 'failed to align' $raw_report | cut -f2 -d ':' | cut -f 2 -d ' '`"
+	echo -e "bowtie1_unmapped_per\t`grep 'failed to align' $raw_report | cut -f2 -d ':' |  cut -f 3 -d ' ' | tr -d "("| tr -d ")"| tr -d "%"`"
+	echo -e "bowtie1_mapped_per\t`grep 'reads with at least one reported alignment' $raw_report | cut -f2 -d ':' |  cut -f 3 -d ' ' | tr -d "("| tr -d ")"| tr -d "%"`"
+	echo -e "bowtie1_mapped\t`grep 'reads with at least one reported alignment' $raw_report | cut -f2 -d ':' | cut -f 2 -d ' '`"
+	echo -e "bowtie1_filtered_reads\t`grep 'reads processed' $raw_report | cut -f2 -d ':' | tr -d " "`"
+fi
