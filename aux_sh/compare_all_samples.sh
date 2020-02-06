c0cdd2d4ad88ea6ef21acc9ab13cb3b618089c83
elenarojano <elenarojano@uma.es>
2020-02-06 11:40:07 +0100

    cd-hit-est parameter -T corrected from 2 to 1

--- aux_sh/compare_all_samples.sh
+++ aux_sh/compare_all_samples.sh
@@ -12,7 +12,7 @@ if [[ $experiment_type == "miRNAseq_detection" ]]; then
 	module load cdhit
 	## miRNA result unificaton and comparison in report
 	cat $MAPPING_RESULTS_FOLDER/*/miRDeep2.pl_0000/total_miRNAs.fasta > $mapping_ref'/all_miRNA.fasta'
-	cd-hit-est -T 2 -M 0 -i $mapping_ref'/all_miRNA.fasta' -o $mapping_ref'/miRNA_nr.fasta' -c 1
+	cd-hit-est -T 1 -M 0 -i $mapping_ref'/all_miRNA.fasta' -o $mapping_ref'/miRNA_nr.fasta' -c 1
 	report_html -t $REPORT_TEMPLATES_FOLDER/all_miRNA_report.erb -d $report_folder/metric_table -o $report_folder/all_miRNA_report
 
 elif [[ $experiment_type == "RNAseq_genome" || $experiment_type == "RNAseq_transcriptome" ]];then
