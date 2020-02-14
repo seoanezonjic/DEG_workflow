6281829782b5c83a39a5603ceade803cc058cd5c
JoseCorCab <josecordoba@uma.es>
2020-02-14 13:32:07 +0100

    DEGenes Hunter commands are now printed to log

--- aux_sh/compare_all_samples.sh
+++ aux_sh/compare_all_samples.sh
@@ -31,7 +31,7 @@ elif [[ $experiment_type == "RNAseq_genome" || $experiment_type == "RNAseq_trans
 		grep -v '^N_' $target_results_folder'/selected_counts' | sum_counts_by_isoform.rb - > $target_results_folder'/final_counts.txt'
 
 		## Launch DEGenesHunter
-		echo $degenes_hunter_options > $target_results_folder/'degenes_Hunter.log'
+		echo degenes_Hunter.R $degenes_hunter_options -i $target_results_folder'/final_counts.txt' -t $TARGET_FILE -o $target_results_folder > $target_results_folder/'degenes_Hunter.log'
 		/usr/bin/time -o $target_results_folder/process_data -v degenes_Hunter.R $degenes_hunter_options -i $target_results_folder'/final_counts.txt' -t $TARGET_FILE -o $target_results_folder &>>$target_results_folder/'degenes_Hunter.log' &
 	done
 	wait
--- aux_sh/launch_fun_hun.sh
+++ aux_sh/launch_fun_hun.sh
@@ -8,7 +8,7 @@ for TARGET_FILE in `echo $TARGETS | tr "," " "`
 do
 	TARGET_NAME=`echo $TARGET_FILE | sed 's/_target.txt//'`
 	target_results_folder=$HUNTER_RESULTS_FOLDER'/'$TARGET_NAME
-	echo $degenes_hunter_options > $target_results_folder/'functional_Hunter.log'
-        /usr/bin/time -o $target_results_folder/process_data -v functional_Hunter.R $functional_hunter_options -i $target_results_folder -t E -o $target_results_folder/functional_enrichment &>$target_results_folder'/functional_Hunter.log' &
+	echo functional_Hunter.R $functional_hunter_options -i $target_results_folder -t E -o $target_results_folder/functional_enrichment > $target_results_folder/'functional_Hunter.log'
+	/usr/bin/time -o $target_results_folder/process_data -v functional_Hunter.R $functional_hunter_options -i $target_results_folder -t E -o $target_results_folder/functional_enrichment &>$target_results_folder'/functional_Hunter.log' &
 done
 wait
