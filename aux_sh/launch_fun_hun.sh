efd38d14761df5a53f672f3c7204e10740b388c4
JoseCorCab <josecordoba@uma.es>
2020-02-18 14:06:13 +0100

    Multicore execution habilitated for functional Hunter

--- aux_sh/launch_fun_hun.sh
+++ aux_sh/launch_fun_hun.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 #SBATCH --mem='20gb'
+#SBATCH --cpus=6
 hostname
 
 source ~soft_bio_267/initializes/init_degenes_hunter
+
 functional_hunter_options=`generate_DGHunter_command.rb -m "functional_Hunter"`
 for TARGET_FILE in `echo $TARGETS | tr "," " "`
 do
 	TARGET_NAME=`echo $TARGET_FILE | sed 's/_target.txt//'`
 	target_results_folder=$HUNTER_RESULTS_FOLDER'/'$TARGET_NAME
-	/usr/bin/time -o $target_results_folder/process_data -v functional_Hunter.R $functional_hunter_options -i $target_results_folder -t E -o $target_results_folder/functional_enrichment &>$target_results_folder'/functional_Hunter.log' &
+	/usr/bin/time -o $target_results_folder/process_data -v functional_Hunter.R -c 6 $functional_hunter_options -i $target_results_folder -t E -o $target_results_folder/functional_enrichment &>$target_results_folder'/functional_Hunter.log'
 done
 wait
