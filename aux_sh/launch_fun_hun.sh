f96e3cf95b97763b9d83f12a10b226ff179a0f39
JoseCorCab <josecordoba@uma.es>
2020-02-25 15:07:11 +0100

    Specific clusters report independent of functional Hunter has been added to 'daemon.sh 4b' mode. Now functional hunter is launched in 'daemon.sh 4a' mode

--- aux_sh/launch_fun_hun.sh
+++ aux_sh/launch_fun_hun.sh
@@ -2,17 +2,17 @@
 #SBATCH --mem='20gb'
 #SBATCH --cpus=6
 hostname
-
+functional_launch=$1
 source ~soft_bio_267/initializes/init_degenes_hunter
 functional_hunter_options=`generate_DGHunter_command.rb -m "functional_Hunter"`
 for TARGET_FILE in `echo $TARGETS | tr "," " "`
 do
 	TARGET_NAME=`echo $TARGET_FILE | sed 's/_target.txt//'`
 	target_results_folder=$HUNTER_RESULTS_FOLDER'/'$TARGET_NAME
-	if [ $experiment_type == "Clusters_report" ]; then 
-		/usr/bin/time -o $target_results_folder/process_data_cluster_report -v render_corr_report.R -i $target_results_folder -o $target_results_folder/functional_enrichment &>$target_results_folder'/functional_Hunter.log'
-	else
+	if [ "$functional_launch" == "4a" ]; then 
 		/usr/bin/time -o $target_results_folder/process_data_functional_hunter -v functional_Hunter.R -c 6 $functional_hunter_options -i $target_results_folder -t E -o $target_results_folder/functional_enrichment &>$target_results_folder'/functional_Hunter.log'
+	elif [ "$functional_launch" == "4b" ]; then 
+		/usr/bin/time -o $target_results_folder/process_data_cluster_report -v render_corr_report.R -i $target_results_folder -o $target_results_folder/functional_enrichment &>$target_results_folder'/functional_Hunter.log'
 	fi
 done
 wait
--- daemon.sh
+++ daemon.sh
@@ -2,6 +2,7 @@
 
 framework_dir=`dirname $0`
 export CODE_PATH=$(readlink -f $framework_dir )
+module=$1
 export ADD_OPTIONS=$2
 CONFIG_DAEMON=$CODE_PATH'/config_daemon'
 if [ "$3" != "" ] ; then
@@ -11,6 +12,8 @@ export PATH=$CODE_PATH'/aux_sh:'$PATH
 export PATH=$CODE_PATH'/aux_parsers:'$PATH
 
 source $CONFIG_DAEMON
+eval "$generate_targets"
+export TARGETS=`ls $TARGETS_FOLDER/*_target.txt | rev | cut -f 1 -d "/" | rev | tr "\n" ","` ; TARGETS=${TARGETS%?}	#-------#	Target file location, including a short sample description	
 n_target=`echo $TARGETS |tr "," "\n" | wc -l `
 tasks=`echo $n_target"+1" | bc`
 
@@ -19,12 +22,12 @@ tasks=`echo $n_target"+1" | bc`
 mkdir -p  $MAPPING_RESULTS_FOLDER
 
 
-if [ "$1" == "1" ] ; then
+if [ "$module" == "1" ] ; then
 	#STAGE 1 DOWNLOADING REFERENCE
 	echo "Launching stage 1: Downloading reference"
 	download_files_for_index.sh 
 
-elif [ "$1" == "1b" ] ; then
+elif [ "$module" == "1b" ] ; then
 	#STAGE 1 INDEXING REFERENCE
 	echo "Launching stage 1: Indexing reference"
 	if [ $launch_login == TRUE ]; then	
@@ -33,31 +36,36 @@ elif [ "$1" == "1b" ] ; then
 		sbatch create_index.sh
 	fi
 
-elif [ "$1" == "2" ] ; then
+elif [ "$module" == "2" ] ; then
 	#STAGE 2 TRIMMING AND MAPPING SAMPLES
 	echo "Launching stage 2: Trimming and mapping samples"
 	trim_and_map.sh
 
-elif [ "$1" == "2b" ] ; then
+elif [ "$module" == "2b" ] ; then
 	check_wf.sh
 
-elif [ "$1" == "3" ] ; then
+elif [ "$module" == "3" ] ; then
 	#STAGE 3 SAMPLES COMPARISON
 	echo "Launching stage 3: Comparing samples"
-	eval "$generate_targets"
-	export TARGETS=`ls $TARGETS_FOLDER/*_target.txt | rev | cut -f 1 -d "/" | rev | tr "\n" ","` ; TARGETS=${TARGETS%?}	#-------#	Target file location, including a short sample description	
 	if [ $launch_login == TRUE ]; then
 		compare_all_samples.sh
 	else
 		sbatch -c $tasks compare_all_samples.sh
 	fi
-elif [ "$1" == "4" ] ; then
-	#STAGE 4 : FUNCTIONAL ANALYSIS
-	echo "Launching stage 4: Functional analysis"
-
+elif [ "$module" == "4a" ] ; then
+	#STAGE 4A : FUNCTIONAL ANALYSIS
+	echo "Launching stage 4a: Launching functional hunter"
 	if [ `echo $fun_remote_mode | grep -q -G "[kb]"` ] || [ $launch_login == TRUE ]; then	
-		launch_fun_hun.sh
+		launch_fun_hun.sh $module
 	else
-		sbatch -c $tasks launch_fun_hun.sh	
+		sbatch -c $tasks launch_fun_hun.sh $module	
+	fi
+elif [ "$module" == "4b" ]; then
+	#STAGE 4B : Creating Clusters specific report
+	echo "Launching stage 4b: Creating Cluster specific report"
+	if [ $launch_login == TRUE ]; then	
+		launch_fun_hun.sh $module
+	else
+		sbatch -c $tasks launch_fun_hun.sh $module
 	fi
 fi
