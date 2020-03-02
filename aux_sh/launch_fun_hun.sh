0db539c01e0f5fb60bed4c3d4d4c8f554d0690aa
JoseCorCab <josecordoba@uma.es>
2020-03-02 10:42:58 +0100

    functional Hunter results folder now is removed before execution

--- aux_sh/launch_fun_hun.sh
+++ aux_sh/launch_fun_hun.sh
@@ -11,6 +11,7 @@ for TARGET_FILE in `echo $TARGETS | tr "," " "`
 do
 	TARGET_NAME=`echo $TARGET_FILE | sed 's/_target.txt//'`
 	target_results_folder=$HUNTER_RESULTS_FOLDER'/'$TARGET_NAME
+	rm -r $target_results_folder/functional_enrichment
 	if [ "$functional_launch" == "4a" ]; then 
 		/usr/bin/time -o $target_results_folder/process_data_functional_hunter -v functional_Hunter.R -c 6 $functional_hunter_options -i $target_results_folder -t E -o $target_results_folder/functional_enrichment &>$target_results_folder'/functional_Hunter.log'
 	elif [ "$functional_launch" == "4b" ]; then 
