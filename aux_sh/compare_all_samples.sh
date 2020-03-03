ec1192053b034b97a35e2a9c1010ee1569af5d59
JoseCorCab <josecordoba@uma.es>
2020-03-03 12:42:11 +0100

    test commit

--- aux_sh/compare_all_samples.sh
+++ aux_sh/compare_all_samples.sh
@@ -15,7 +15,7 @@ create_report.R -t $REPORT_TEMPLATES_FOLDER/alignments_report.Rmd -o $report_fol
 if [[ $experiment_type == "miRNAseq_detection" ]]; then
 	. ~soft_bio_267/initializes/init_ruby
 	module load cdhit
-	## miRNA result unificaton and comparison in report
+	## miRNA result unificaton and comparison in report # test
 	cat $MAPPING_RESULTS_FOLDER/*/miRDeep2.pl_0000/total_miRNAs.fasta > $mapping_ref'/all_miRNA.fasta'
 	cd-hit-est -T 1 -M 0 -i $mapping_ref'/all_miRNA.fasta' -o $mapping_ref'/miRNA_nr.fasta' -c 1
 	report_html -t $REPORT_TEMPLATES_FOLDER/all_miRNA_report.erb -d $report_folder/metric_table -o $report_folder/all_miRNA_report
