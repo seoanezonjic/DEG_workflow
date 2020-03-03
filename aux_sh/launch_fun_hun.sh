eb7e85c2b9c72471d1aa0f11bb2ef4d2d54e09c5
JoseCorCab <josecordoba@uma.es>
2020-03-03 09:54:08 +0100

    A bug in functional hunter script has been fixed

--- aux_sh/launch_fun_hun.sh
+++ aux_sh/launch_fun_hun.sh
@@ -9,7 +9,7 @@ source ~soft_bio_267/initializes/init_degenes_hunter
 functional_hunter_options=`generate_DGHunter_command.rb -m "functional_Hunter"`
 for TARGET_FILE in `echo $TARGETS | tr "," " "`
 do
-	TARGET_N AME=`echo $TARGET_FILE | sed 's/_target.txt//'`
+	TARGET_NAME=`echo $TARGET_FILE | sed 's/_target.txt//'`
 	target_results_folder=$HUNTER_RESULTS_FOLDER'/'$TARGET_NAME
 	rm -r $target_results_folder/functional_enrichment
 	if [ "$functional_launch" == "4a" ]; then 
