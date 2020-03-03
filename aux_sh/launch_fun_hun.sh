0264de444c50c287bc3fa043139d00aa8c17e1e6
JoseCorCab <josecordoba@uma.es>
2020-03-03 09:36:26 +0100

    Distributed functional hunter disabled

--- aux_sh/launch_fun_hun.sh
+++ aux_sh/launch_fun_hun.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #SBATCH --mem='20gb'
-#SBATCH --cpus=6
+#SBATCH --cpus=3
 #SBATCH --constraint=cal
 
 hostname
@@ -9,11 +9,11 @@ source ~soft_bio_267/initializes/init_degenes_hunter
 functional_hunter_options=`generate_DGHunter_command.rb -m "functional_Hunter"`
 for TARGET_FILE in `echo $TARGETS | tr "," " "`
 do
-	TARGET_NAME=`echo $TARGET_FILE | sed 's/_target.txt//'`
+	TARGET_N AME=`echo $TARGET_FILE | sed 's/_target.txt//'`
 	target_results_folder=$HUNTER_RESULTS_FOLDER'/'$TARGET_NAME
 	rm -r $target_results_folder/functional_enrichment
 	if [ "$functional_launch" == "4a" ]; then 
-		/usr/bin/time -o $target_results_folder/process_data_functional_hunter -v functional_Hunter.R -c 6 $functional_hunter_options -i $target_results_folder -t E -o $target_results_folder/functional_enrichment &>$target_results_folder'/functional_Hunter.log'
+		/usr/bin/time -o $target_results_folder/process_data_functional_hunter -v functional_Hunter.R -c 3 $functional_hunter_options -i $target_results_folder -t E -o $target_results_folder/functional_enrichment &>$target_results_folder'/functional_Hunter.log'
 	elif [ "$functional_launch" == "4b" ]; then 
 		/usr/bin/time -o $target_results_folder/process_data_cluster_report -v render_corr_report.R -i $target_results_folder -o $target_results_folder/functional_enrichment &>$target_results_folder'/functional_Hunter.log'
 	fi
