0607005f760e1c7ccab593fbfff5dced2ff7baf1
JoseCorCab <josecordoba@uma.es>
2020-03-26 16:13:29 +0100

    Debug mode activated by default inWorkflow

--- aux_sh/launch_fun_hun.sh
+++ aux_sh/launch_fun_hun.sh
@@ -6,6 +6,7 @@
 hostname
 functional_launch=$1
 source ~soft_bio_267/initializes/init_degenes_hunter
+export PATH=~/software/merge/DEgenesHunter:$PATH
 functional_hunter_options=`generate_DGHunter_command.rb -m "functional_Hunter"`
 for TARGET_FILE in `echo $TARGETS | tr "," " "`
 do
@@ -13,7 +14,7 @@ do
 	target_results_folder=$HUNTER_RESULTS_FOLDER'/'$TARGET_NAME
 	rm -r $target_results_folder/functional_enrichment
 	if [ "$functional_launch" == "4a" ]; then 
-		/usr/bin/time -o $target_results_folder/process_data_functional_hunter -v functional_Hunter.R $functional_hunter_options -i $target_results_folder -t E -o $target_results_folder/functional_enrichment &>$target_results_folder'/functional_Hunter.log' &#-c 6
+		/usr/bin/time -o $target_results_folder/process_data_functional_hunter -v functional_Hunter.R $functional_hunter_options -i $target_results_folder -t E --Debug $target_results_folder/debug_files/DH_debug_session.RData -o $target_results_folder/functional_enrichment &>$target_results_folder'/functional_Hunter.log' #&#-c 6
 	elif [ "$functional_launch" == "4b" ]; then 
 		/usr/bin/time -o $target_results_folder/process_data_cluster_report -v render_corr_report.R -i $target_results_folder -o $target_results_folder/functional_enrichment &>$target_results_folder'/functional_Hunter.log' &
 	fi
