11cace7534dd2db9d273853917f4f523318b1926
JoseCorCab <josecordoba@uma.es>
2020-06-18 11:36:37 +0200

    known miRNA translation method added to workflow

--- aux_sh/compare_all_samples.sh
+++ aux_sh/compare_all_samples.sh
@@ -36,6 +36,7 @@ if [[ $experiment_type == "miRNAseq_detection" ]]; then
 	module load cdhit
 	## miRNA result unificaton and comparison in report # test
 	cat $MAPPING_RESULTS_FOLDER/*/miRDeep2.pl_0000/total_miRNAs.fasta > $mapping_ref'/all_miRNA.fasta'
+	cat $MAPPING_RESULTS_FOLDER/*/miRDeep2.pl_0000/known_miRNAs.translation | sort -u > $mapping_ref'/known_miRNAs.translation'
 	cd-hit-est -T 1 -M 0 -i $mapping_ref'/all_miRNA.fasta' -o $mapping_ref'/miRNA_nr.fasta' -c 1
 	report_html -t $REPORT_TEMPLATES_FOLDER/all_miRNA_report.erb -d $report_folder/metric_table -o $report_folder/all_miRNA_report
 
