47df25a78efa782cf662246fae32496a829931fb
elenarojano <elenarojano@uma.es>
2023-07-11 14:41:05 +0200

    Changes in map_too_short.sh. Refactoring needed for single reads.

--- aux_sh/map_too_short.sh
+++ aux_sh/map_too_short.sh
@@ -10,22 +10,11 @@ hostname
 
 module load star/2.5.3a
 
-STAR --runThreadN 32 --genomeDir /mnt/home/users/pab_001_uma/pedro/references/hsGRc38/STAR_index --readFilesIn /mnt2/fscratch/users/bio_267_uma/elenarojano/NGS_projects/sarcoma/results/clean_and_map/CTL_1_cell/seqtrimbb_0000/output_files/paired_1.fastq.gz /mnt2/fscratch/users/bio_267_uma/elenarojano/NGS_projects/sarcoma/results/clean_and_map/CTL_1_cell/seqtrimbb_0000/output_files/paired_2.fastq.gz --readFilesCommand 'zcat' --quantMode TranscriptomeSAM GeneCounts --outSAMtype BAM SortedByCoordinate --outReadsUnmapped Fastx
-exit
-
-if  [ $1 == 'paired' ] ;then
-	# Paired
-	STAR --runThreadN 32 --genomeDir /mnt/home/users/pab_001_uma/pedro/references/hsGRc38/STAR_index --readFilesIn $2 $3 --readFilesCommand 'zcat' --quantMode TranscriptomeSAM GeneCounts --outSAMtype BAM SortedByCoordinate --outReadsUnmapped Fastx
-	STAR --runThreadN 32 --genomeDir /mnt/home/users/pab_001_uma/pedro/references/hsGRc38/STAR_index --readFilesIn Unmapped.out.mate1 Unmapped.out.mate2 --outSAMtype BAM SortedByCoordinate --outFilterScoreMinOverLread 0 --outFilterMatchNminOverLread 0
-
-elif [ $1 == 'single' ] ;then
-	# Single
-	STAR --runThreadN 32 --genomeDir /mnt/home/users/pab_001_uma/pedro/references/hsGRc38/STAR_index --readFilesIn $2 --readFilesCommand 'zcat' --quantMode TranscriptomeSAM GeneCounts --outSAMtype BAM SortedByCoordinate --outReadsUnmapped Fastx
-	STAR --runThreadN 32 --genomeDir /mnt/home/users/pab_001_uma/pedro/references/hsGRc38/STAR_index --readFilesIn Unmapped.out.mate1 --outSAMtype BAM SortedByCoordinate --outFilterScoreMinOverLread 0 --outFilterMatchNminOverLread	
-fi
+STAR --runThreadN 32 --genomeDir /mnt/home/users/pab_001_uma/pedro/references/hsGRc38/STAR_index --readFilesIn /mnt2/fscratch/users/bio_267_uma/elenarojano/NGS_projects/sarcoma/results/clean_and_map/CTL_2_cell/seqtrimbb_0000/output_files/paired_1.fastq.gz /mnt2/fscratch/users/bio_267_uma/elenarojano/NGS_projects/sarcoma/results/clean_and_map/CTL_2_cell/seqtrimbb_0000/output_files/paired_2.fastq.gz --readFilesCommand 'zcat' --quantMode TranscriptomeSAM GeneCounts --outSAMtype BAM SortedByCoordinate --outReadsUnmapped Fastx
+STAR --runThreadN 32 --genomeDir /mnt/home/users/pab_001_uma/pedro/references/hsGRc38/STAR_index --readFilesIn Unmapped.out.mate1 Unmapped.out.mate2 --outSAMtype BAM SortedByCoordinate --outFilterScoreMinOverLread 0 --outFilterMatchNminOverLread 0
 
 module unload star/2.5.3a
 module load samtools/1.3
+
 samtools index Aligned.sortedByCoord.out.bam
 
-# Note: open file with IGV to track coordinates
