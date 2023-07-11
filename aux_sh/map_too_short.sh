43166e23d8469aa40b2cc1d9414d366018c2577a
elenarojano <elenarojano@uma.es>
2023-07-11 14:26:52 +0200

    Minor corrections in map_too_short.sh

--- aux_sh/map_too_short.sh
+++ aux_sh/map_too_short.sh
@@ -5,26 +5,27 @@
 #SBATCH --constraint=cal
 #SBATCH --error=job.%J.err
 #SBATCH --output=job.%J.out
+
 hostname
 
-module load samtools/1.3
 module load star/2.5.3a
 
-# example line
-#STAR --runThreadN 32 --genomeDir /mnt/home/users/pab_001_uma/pedro/references/hsGRc38/STAR_index --readFilesIn /mnt2/fscratch/users/bio_267_uma/elenarojano/NGS_projects/sarcoma/results/clean_and_map/CTL_1_cell/seqtrimbb_0000/output_files/paired_1.fastq.gz /mnt2/fscratch/users/bio_267_uma/elenarojano/NGS_projects/sarcoma/results/clean_and_map/CTL_1_cell/seqtrimbb_0000/output_files/paired_2.fastq.gz --readFilesCommand 'zcat' --quantMode TranscriptomeSAM GeneCounts --outSAMtype BAM SortedByCoordinate --outReadsUnmapped Fastx
+STAR --runThreadN 32 --genomeDir /mnt/home/users/pab_001_uma/pedro/references/hsGRc38/STAR_index --readFilesIn /mnt2/fscratch/users/bio_267_uma/elenarojano/NGS_projects/sarcoma/results/clean_and_map/CTL_1_cell/seqtrimbb_0000/output_files/paired_1.fastq.gz /mnt2/fscratch/users/bio_267_uma/elenarojano/NGS_projects/sarcoma/results/clean_and_map/CTL_1_cell/seqtrimbb_0000/output_files/paired_2.fastq.gz --readFilesCommand 'zcat' --quantMode TranscriptomeSAM GeneCounts --outSAMtype BAM SortedByCoordinate --outReadsUnmapped Fastx
+exit
 
-if  [[ $1=='paired' ]]
-then
+if  [ $1 == 'paired' ] ;then
 	# Paired
 	STAR --runThreadN 32 --genomeDir /mnt/home/users/pab_001_uma/pedro/references/hsGRc38/STAR_index --readFilesIn $2 $3 --readFilesCommand 'zcat' --quantMode TranscriptomeSAM GeneCounts --outSAMtype BAM SortedByCoordinate --outReadsUnmapped Fastx
 	STAR --runThreadN 32 --genomeDir /mnt/home/users/pab_001_uma/pedro/references/hsGRc38/STAR_index --readFilesIn Unmapped.out.mate1 Unmapped.out.mate2 --outSAMtype BAM SortedByCoordinate --outFilterScoreMinOverLread 0 --outFilterMatchNminOverLread 0
 
-elif [[ $1=='single' ]]
+elif [ $1 == 'single' ] ;then
 	# Single
 	STAR --runThreadN 32 --genomeDir /mnt/home/users/pab_001_uma/pedro/references/hsGRc38/STAR_index --readFilesIn $2 --readFilesCommand 'zcat' --quantMode TranscriptomeSAM GeneCounts --outSAMtype BAM SortedByCoordinate --outReadsUnmapped Fastx
 	STAR --runThreadN 32 --genomeDir /mnt/home/users/pab_001_uma/pedro/references/hsGRc38/STAR_index --readFilesIn Unmapped.out.mate1 --outSAMtype BAM SortedByCoordinate --outFilterScoreMinOverLread 0 --outFilterMatchNminOverLread	
 fi
 
+module unload star/2.5.3a
+module load samtools/1.3
 samtools index Aligned.sortedByCoord.out.bam
 
 # Note: open file with IGV to track coordinates
