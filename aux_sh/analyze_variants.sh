77f844b225b408f4a76a2fae75c0920151dcb7c6
Álvaro Esteban Martos <alvaroesteban@uma.es>
2025-05-20 16:24:11 +0200

    Variant analysis template and report now include effect prediction step

---
+++ aux_sh/analyze_variants.sh
@@ -0,0 +1,26 @@
+#! /usr/bin/env bash
+#SBATCH --mem='60gb'
+#SBATCH --constraint=cal
+#SBATCH --time='02:00:00'
+hostname
+
+echo "Preparing multiVCF"
+mkdir -p $VARIANT_RESULTS_FOLDER
+module load bcftools/1.21
+source ~soft_bio_267/initializes/init_htmlreportR
+vcf=`find $MAPPING_RESULTS_FOLDER/*/gatk_0000/filtered.vcf.gz`
+bcftools merge $vcf --output-type z --output $VARIANT_RESULTS_FOLDER/"combined.vcf.gz"
+header=`bcftools view -h $VARIANT_RESULTS_FOLDER/"combined.vcf.gz" | tail -n 1`
+bcftools view -H $VARIANT_RESULTS_FOLDER/"combined.vcf.gz" -o $VARIANT_RESULTS_FOLDER/"tmp_variants.txt"
+grep PASS $VARIANT_RESULTS_FOLDER/"tmp_variants.txt" > $VARIANT_RESULTS_FOLDER/"all_variants.txt"
+rm $VARIANT_RESULTS_FOLDER/"tmp_variants.txt"
+sed -i "1i $header" $VARIANT_RESULTS_FOLDER/"all_variants.txt"
+sed -i "s/#//g" $VARIANT_RESULTS_FOLDER/"all_variants.txt"
+effects=`find $MAPPING_RESULTS_FOLDER/*/gatk_0000/var_effects.txt`
+rm $VARIANT_RESULTS_FOLDER/"all_variant_effects.txt"
+for effect in $effects; do
+	cat $effect >> $VARIANT_RESULTS_FOLDER/"all_variant_effects.txt"
+done
+echo "Command called: html_report.R -t $REPORT_TEMPLATES_FOLDER'/variants_report.txt' -o $VARIANT_RESULTS_FOLDER'/variants_report.html' -d $VARIANT_RESULTS_FOLDER'/all_variants.txt',$VARIANT_RESULTS_FOLDER'/all_variant_effects.txt' --title 'Variant analysis report'"
+html_report.R -t $REPORT_TEMPLATES_FOLDER'/variants_report.txt' -o $VARIANT_RESULTS_FOLDER'/variants_report.html' -d $VARIANT_RESULTS_FOLDER'/all_variants.txt',$VARIANT_RESULTS_FOLDER'/all_variant_effects.txt' --title 'Variant analysis report'
+echo "Report built in "$VARIANT_RESULTS_FOLDER"/variants_report.html"
